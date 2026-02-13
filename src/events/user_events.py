3b18b04cbf252d93712c5b248fe63276cfa69a9a
Emmanuel <emmanuelnwokoma364@gmail.com>
2026-02-13 02:08:19 +0100

    add an event

--- src/events/user_events.py
+++ src/events/user_events.py
@@ -1,9 +1,13 @@
 from src.events.base import DomainEvent
 
-
-class UserCreatedEvent(DomainEvent):
+class BaseUserEvent(DomainEvent):
     email: str
     first_name: str
     last_name: str
     token: str
 
+class UserCreatedEvent(BaseUserEvent):
+    pass
+
+class RequestPasswordResetEvent(BaseUserEvent):
+    pass
