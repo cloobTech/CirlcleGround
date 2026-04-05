a9883b23813471396a04189e356dfb372339bdb8
Emmanuel <emmanuelnwokoma364@gmail.com>
2026-04-05 10:48:33 +0100

    log activities

--- test.py
+++ test.py
@@ -1,28 +1,28 @@
 from src.storage import db
 import asyncio
 
-from src.models.conversation import Conversation
+# from src.models.conversation import Conversation
 
-
-async def main(): 
-    new_conversation = Conversation()
-    print(new_conversation.id)
-    async with db.get_session() as session:
-        session.add(new_conversation)
-        await session.commit()
-        
-asyncio.run(main())
-
-# from src.models import user, booking, space, location, space_addon, space_amenities, space_image, space_pricing, space_rule, space_use_case, amenities, custom_amenity, payments, reviews
 
 # async def main(): 
-       
-#     try:
-#         await db.drop_tables()
-#         await db.create_tables()
-
-#     except ValueError as e:
-#         print(e)
+#     new_conversation = Conversation()
+#     print(new_conversation.id)
+#     async with db.get_session() as session:
+#         session.add(new_conversation)
+#         await session.commit()
         
 # asyncio.run(main())
+
+from src.models import user, booking, space, location, space_addon, space_amenities, space_image, space_pricing, space_rule, space_use_case, amenities, custom_amenity, payments, reviews
+
+async def main(): 
+       
+    try:
+        await db.drop_tables()
+        await db.create_tables()
+
+    except ValueError as e:
+        print(e)
+        
+asyncio.run(main())
 
